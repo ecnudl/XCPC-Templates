7bd4aae5c1700c1bd252f9727b6133d19f569c8e
ecnudl <2629717221@qq.com>
2024-11-23 21:36:21 +0800

    modify 11.23

--- templates/数据结构/并查集.cpp
+++ templates/数据结构/并查集.cpp
@@ -26,3 +26,21 @@ struct DSU {
         return find(x) == find(y);
     }
 };
+
+//可撤销并查集
+struct DSU{
+    int n = 0, tot = 0, fa[N], sz[N], s[N * 20];
+    void ins() { n++, fa[n] = n, sz[n] = 1;}//插入节点
+    int F(int x) { return fa[x] == x ? x : F(fa[x]); }//即find查找函数
+    void U(int x, int y){//合并函数
+        x = F(x), y = F(y);
+        if(x == y) return;
+        if(sz[x] < sz[y]) swap(x, y);
+        s[++tot] = y, fa[y] = x, sz[x] += sz[y];
+    }
+    void D(){//删除栈顶边
+        if(!tot) return;
+        int y = s[tot--]; sz[fa[y]] -= sz[y], fa[y] = y;
+    }
+    void back(int t = 0){while(tot > t) D();}//删除到只剩t条边
+}dsu;
