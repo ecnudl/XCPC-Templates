f5da3f525798cfcc627627947fd0812bd02b5d0e
ecnudl <2629717221@qq.com>
2024-11-29 08:39:18 +0800

    modify 11.29

--- templates/数据结构/主席树.cpp
+++ templates/数据结构/主席树.cpp
@@ -65,3 +65,102 @@ int main() {
     work();
     return 0;
 }
+
+//主席树板子题
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endl '\n'
+
+const int N = 3e5 + 10;
+int n, m, node, rt[N], val[N << 5], ls[N << 5], rs[N << 5], a[N];
+
+void pushup(int u) {
+    val[u] = val[ls[u]] + val[rs[u]];
+}
+
+void ins(int &u, int pre, int l, int r, int x) {
+    u = ++node;
+    val[u] = val[pre];
+    ls[u] = ls[pre], rs[u] = rs[pre];
+    if(l == r) {
+        val[u]++;
+        return;
+    }
+    int mid = (l + r) >> 1;
+    if(x <= mid) ins(ls[u], ls[pre], l, mid, x);
+    else ins(rs[u], rs[pre], mid + 1, r, x);
+    pushup(u);
+}
+
+int query(int u, int pre, int k, int l, int r) {  //实现了查询区间第k大
+    if(l == r) return l;
+    int mid = (l + r) >> 1;
+    int siz = val[ls[u]] - val[ls[pre]];
+    if(siz < k) return query(rs[u], rs[pre], k - siz, mid + 1, r);
+    else return query(ls[u], ls[pre], k, l, mid);
+}
+
+int check(int u, int pre, int x, int l, int r) { //查询某个数字在区间中出现的次数
+    if(l == r) return val[u] - val[pre];
+    int mid = (l + r) >> 1;
+    if(x <= mid) return check(ls[u], ls[pre], x, l, mid);
+    else return check(  rs[u], rs[pre], x, mid + 1, r);
+}
+
+void solve() {
+    cin >> n >> m;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    for(int i = 1;i <= n;i++) ins(rt[i], rt[i - 1], 1, n, a[i]);
+    while(m--) {
+        int l, r, k;
+        cin >> l >> r >> k;
+        int dis = (r - l + 1) / k + 1, rnk = 1;
+        int ans = -1;
+        while(rnk <= r - l + 1) {
+            int now = query(rt[r], rt[l - 1], rnk, 1, n);
+            if(check(rt[r], rt[l - 1], now, 1, n) >= dis) {
+                ans = now;
+                break;
+            }
+            rnk += dis;
+        }
+        cout << ans << endl;
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+//    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
+
+//查询区间中小于等于某个数的数字之和
+void pushup(int u) {
+    sum[u] = sum[ls[u]] + sum[rs[u]];
+}
+ 
+void modify(int &u, int pre, int l, int r, int pos) {
+    u = ++tot;
+    sum[u] = sum[pre], ls[u] = ls[pre], rs[u] = rs[pre];
+    if(l == r) {
+        sum[u] += pos;
+        return;
+    }
+    int mid = (l + r) >> 1;
+    if(pos <= mid) modify(ls[u], ls[pre], l, mid, pos);
+    else modify(rs[u], rs[pre], mid + 1, r, pos);
+    pushup(u);
+}
+ 
+int query(int u, int pre, int l, int r, int pos) {
+    if(pos >= r) return sum[u] - sum[pre];
+    int mid = (l + r) >> 1;
+    return query(ls[u], ls[pre], l, mid, pos) + (pos > mid ? query(rs[u], rs[pre], mid + 1, r, pos) : 0);
+}
